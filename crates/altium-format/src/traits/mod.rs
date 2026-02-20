056e580cbebe22c436c85d746a175b7d940338f5
Alexander Kiselev <git@akiselev.com>
2026-02-20 09:05:08 -0800

    removed old v1 code

--- crates/altium-format/src/traits/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Core traits for Altium value conversion.
-//!
-//! This module provides traits for converting between Rust types and Altium parameter values.
-
-mod conversion;
-
-pub use conversion::{FromParamList, FromParamValue, ToParamList, ToParamValue};
